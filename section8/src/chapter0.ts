cdf998f62867d089810322677602026fb93f18fa
YYJ <yoon20220823@dukcung.ac.kr>
2025-05-04 20:24:34 +0900

    [Section 8] 제네릭

---
+++ section8/src/chapter0.ts
@@ -0,0 +1,14 @@
+/*
+ * 제네릭
+ */
+
+// 제네릭 함수
+function func<T>(value: T): T {
+    return value;
+}
+
+let num = func(10);
+let bool = func(true);
+let str = func("string");
+let arr = func([1, 2, 3] as [number, number, number]);
+let arr2 = func<[number, number, number]>([1, 2, 3]);
